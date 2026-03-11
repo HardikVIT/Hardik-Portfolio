f10aa6f7e63393a4577b15bca1760c91b4c2cc1c
Your Name <hardik.sondhi2022@vitstudent.ac.in>
2026-03-11 15:11:52 +0530

    minor-changes

--- src/sections/Certifications.jsx
+++ src/sections/Certifications.jsx
@@ -4,7 +4,7 @@ function Certifications() {
   return (
     <Element
       name="certifications"
-      className="h-screen  text-blue-400 flex items-center justify-center"
+      className="h-screen bg-black text-blue-400 flex items-center justify-center"
     >
       <h1 className="text-5xl text-blue-400">Certifications Section</h1>
     </Element>
